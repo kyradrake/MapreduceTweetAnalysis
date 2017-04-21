5ca9681b4c2145f9b28ed3b2d7d8d1a35bc09655
Colin Banigan <colinbanigan@yahoo.com>
2017-04-21 16:48:00 -0500

    initial hw5 bonus attempt

---
+++ SleepCount.java
@@ -0,0 +1,86 @@
+//example code from the apache website for a simple hadoop application to find the number of a specified word in a text file
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+
+public class SleepCount {
+
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text time = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            
+            /*
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, one);
+            }
+            */
+            
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            
+            //check to see if the line we're on is the time
+            if(itr.nextToken().equals("T")){
+                itr.nextToken();
+                time.set(itr.nextToken().substring(0, 2));
+
+                //check the remaining text for the word sleep
+                Boolean containsSleep = false;
+                while (itr.hasMoreTokens()) {
+                    if(itr.nextToken().equals("sleep")){
+                        containsSleep = true;
+                    }
+                }
+
+                //if the tweet contains sleep, map
+                if(containsSleep){
+                    context.write(time, one);
+                }
+            }
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.setInt(NLineInputFormat.LINES_PER_MAP, 4);
+        Job job = Job.getInstance(conf, "sleep count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setInputFormatClass(NLineInputFormat.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
